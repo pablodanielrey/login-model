1b5ddd0ab58578a1459ad7c52b33f66828262490
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-07-29 23:12:36 -0300

    implemente el login del usuario teniendo en cuenta las eliminaciones de clave

--- src/login/model/LoginModel.py
+++ src/login/model/LoginModel.py
@@ -3,8 +3,6 @@ from .entities.Login import UsuarioClave
 
 class LoginModel:
 
-    def login(self, session, user: str, password: str, device_id: str) {
-        usr = session.query(UsuarioClave).filter(UsuarioClave.usuario == user, UsuarioClave.clave == password).one_or_none()
-        if not usr:
-            
-    }
+    def login(self, session, user: str, password: str, device_id: str):
+        usr = session.query(UsuarioClave).filter(UsuarioClave.usuario == user, UsuarioClave.clave == password, UsuarioClave.eliminada is None).one_or_none()
+        return usr
