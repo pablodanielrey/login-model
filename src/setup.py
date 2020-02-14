38c627625639ddc157d2ff4a0f8921353d4a4a41
Miguel Angel Macagno <miguel.macagno@gmail.com>
2020-02-14 14:29:19 -0300

    Cambio de version

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='login-model',
-          version='1.0.2',
+          version='1.0.3',
           description='Modelo de login',
           url='https://github.com/pablodanielrey/login-model',
           author='Desarrollo DiTeSi, FCE',
