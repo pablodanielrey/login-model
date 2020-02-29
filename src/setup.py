128de28c798c02115907a00c4da48962f87d0fed
Pablo Daniel Rey <pablodanielrey@gmail.com>
2020-02-29 12:03:56 -0300

    cambie la version del docker

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='login-model',
-          version='1.1.7',
+          version='1.1.8',
           description='Modelo de login',
           url='https://github.com/pablodanielrey/login-model',
           author='Desarrollo DiTeSi, FCE',
